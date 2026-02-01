450d9f201a287b4fe6c9364e608e8326ce525445
Samuel Willows <samueltwillows@hotmail.com>
2026-01-31 21:32:19 -0800

    revamp

--- src/api/auth/mod.rs
+++ src/api/auth/mod.rs
@@ -24,9 +24,23 @@ pub(crate) mod signup;
 pub trait Authentication {
 	async fn await_login(
 		&self, auth: LoginAuth,
-	) -> Result<StatusCode, Box<dyn Error + Sync + Send>>;
+	) -> Result<StatusCode, Box<dyn Error + Sync + Send>> {
+		let _ = auth;
+
+		Err(Box::new(std::io::Error::new(
+			std::io::ErrorKind::Other,
+			"await_login not implemented",
+		)))
+	}
 
 	async fn await_signup(
 		&self, auth: SignupAuth,
-	) -> Result<StatusCode, Box<dyn Error + Sync + Send>>;
+	) -> Result<StatusCode, Box<dyn Error + Sync + Send>> {
+		let _ = auth;
+
+		Err(Box::new(std::io::Error::new(
+			std::io::ErrorKind::Other,
+			"await_signup not implemented",
+		)))
+	}
 }
